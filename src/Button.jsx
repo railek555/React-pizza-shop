3b4c7840dc0be27656ad67e8cf830647ef9a91e8
railek555 <railek555@gmail.com>
2022-07-04 20:48:48 +0300

    Added props in Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import classNames from 'classnames';
 
 
 
-class Button extends React.Component {
-    //componentDidMount() {
-    //    console.log('Компонент отоборазился')
-    //}
-    render() {
-        return <button className="button ">{this.props.children}</button>
-    }
+//class Button extends React.Component {
+
+//componentDidMount() {
+//    console.log('Компонент отобразился')
+//}
+
+//   render() { 
+// 1.. return <button className={`button ${this.props.outline ? 'button--outline' : ''} `}>{this.props.children}</button>
+// 2.. return <button className={classNames('button', {
+//    'button--outline': this.props.outline,
+// })}></button>
+// }
+// }
+
+function Button(props) {
+    return (
+        <button className={classNames('button', {
+            'button--outline': props.outline,
+        })}>
+            {props.children}
+        </button>
+
+    );
 }
-
 export default Button;
